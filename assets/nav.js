0b6b11c287db49e41f3f89435e69afc9eb35413c
Anton Strogonoff <anton@strogonoff.name>
2019-04-02 09:14:33 +0800

    feat(nav): Better expandable nav UX. WIP

--- assets/nav.js
+++ assets/nav.js
@@ -19,14 +19,30 @@
   }
 
   class ExpandableContainer {
-    constructor({ containerEl, expandableHtmlClass, expandedHtmlClass }) {
+    constructor({
+        containerEl,
+        headerEl,
+        mainEl,
+        footerEl,
+        expandableNavEl, priorityNavEl,
+        expandableHtmlClass, expandedHtmlClass }) {
+
       this.toggle = this.toggle.bind(this);
 
       this.expandedHtmlClass = expandedHtmlClass;
+
       this.containerEl = containerEl;
+      this.headerEl = headerEl;
+      this.mainEl = mainEl;
+      this.footerEl = footerEl;
+      this.expandableNavEl = expandableNavEl;
+      this.priorityNavEl = priorityNavEl;
+
+      this.expandableNavBottomOffset = 20;
       this.containerEl.classList.add(expandableHtmlClass);
 
       this.expanded = false;
+      this.animationTimeout = undefined;
     }
 
     toggle() {
@@ -36,55 +52,68 @@
 
     update() {
       if (this.expanded) {
-        this.containerEl.classList.add(this.expandedHtmlClass);
-      } else {
-        this.containerEl.classList.remove(this.expandedHtmlClass);
-      }
-    }
-  }
+        window.clearTimeout(this.animationTimeout);
 
-  class SiteHeader {
-    constructor({ headerEl }) {
-      this.headerEl = headerEl;
-      this.expandableNavEl = headerEl.querySelector('nav.expandable-nav');
-      this.priorityNavEl = headerEl.querySelector('nav.priority-nav');
-      this.expandableNavBottomOffset = 20;
-    }
-    expand() {
-      if (this.expandableNavEl) {
+        // Hold main position
+        const mainTopOffset =
+          this.mainEl.getBoundingClientRect().top +
+          document.documentElement.scrollTop -
+          (document.documentElement.clientTop || document.body.clientTop || 0);
+        this.mainEl.style.top = `${mainTopOffset}px`;
+        this.mainEl.style.position = 'absolute';
+        this.mainEl.style.zIndex = '0';
+        this.footerEl.style.display = 'none';
+
+        this.containerEl.classList.add(this.expandedHtmlClass);
+
         const expH = this.expandableNavEl.getBoundingClientRect().height;
         const prioH = this.priorityNavEl.getBoundingClientRect().height;
+        const heightDifference = expH - prioH + this.expandableNavBottomOffset;
 
-        headerEl.style.paddingBottom = `${expH - prioH + this.expandableNavBottomOffset}px`;
-        console.debug(headerEl.style.paddingBottom);
-      }
-    }
-    collapse() {
-      if (this.expandableNavEl) {
-        headerEl.style.removeProperty('padding-bottom');
+        this.headerEl.style.paddingBottom = `${heightDifference}px`;
+        this.headerEl.style.zIndex = '2';
+
+      } else {
+        this.containerEl.classList.remove(this.expandedHtmlClass);
+        this.headerEl.style.removeProperty('padding-bottom');
+
+        window.clearTimeout(this.animationTimeout);
+
+        this.animationTimeout = window.setTimeout(() => {
+          this.mainEl.style.removeProperty('z-index');
+          this.mainEl.style.removeProperty('position');
+          this.mainEl.style.removeProperty('top');
+          this.headerEl.style.removeProperty('z-index');
+          this.footerEl.style.removeProperty('display');
+        }, 1000);
       }
     }
   }
 
   const body = document.querySelector('body');
   const headerEl = document.querySelector('body > header');
+  const expandableNavEl = headerEl.querySelector('nav.expandable-nav');
 
-  const header = new SiteHeader({ headerEl: headerEl });
+  if (expandableNavEl) {
 
-  const container = new ExpandableContainer({
-    containerEl: body,
-    expandableHtmlClass: 'with-expandable-nav',
-    expandedHtmlClass: 'with-expanded-nav',
-  });
+    const container = new ExpandableContainer({
+      containerEl: body,
+      expandableHtmlClass: 'with-expandable-nav',
+      expandedHtmlClass: 'with-expanded-nav',
+      headerEl: headerEl,
+      footerEl: document.querySelector('body > footer'),
+      mainEl: document.querySelector('body > main'),
+      expandableNavEl: expandableNavEl,
+      priorityNavEl: headerEl.querySelector('nav.priority-nav'),
+    });
 
-  const trigger = new NavTrigger({
-    triggerTemplateSelector: '#expandableNavTrigger',
-    onTriggerClick: () => {
-      container.toggle();
-      container.expanded ? header.expand() : header.collapse();
-    },
-  });
+    const trigger = new NavTrigger({
+      triggerTemplateSelector: '#expandableNavTrigger',
+      onTriggerClick: container.toggle,
+    });
 
-  headerEl.appendChild(trigger.render());
+    headerEl.appendChild(trigger.render());
+
+  }
 
 }());
